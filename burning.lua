0ae131543818ec64c2d5981123804372bfca0d35
B. Stack <bgstack15@gmail.com>
2026-03-09 10:31:37 -0400

    fix #1, only make cooling sound when wearing any part of suit

--- burning.lua
+++ burning.lua
@@ -43,15 +43,16 @@ minetest.register_globalstep(function(dtime)
 			local has_chestplate = inv:contains_item("armor","molten_sailor_mcl:chestplate_obsidian")
 			local has_pants = inv:contains_item("armor","molten_sailor_mcl:leggings_obsidian")
 			local has_boots = inv:contains_item("armor","molten_sailor_mcl:boots_obsidian")
+			local has_full_suit = molten_sailor_mcl.has_full_lava_suit(player)
 
 			-- does the player wear a suit?
-			molten_sailor_mcl.set_player_wearing(player, molten_sailor_mcl.has_full_lava_suit(player), has_helmet, armor_inv)
+			molten_sailor_mcl.set_player_wearing(player, has_full_suit, has_helmet, armor_inv)
 
 			-- is player on fire?
 			local pos = player:get_pos()
 			local on_fire = minetest.find_node_near(pos, 1, {"group:lava","group:fire"})
 
-			if on_fire then
+			if on_fire and (has_full_suit or has_helmet or has_chestplate or has_pants or has_boots) then
 				minetest.sound_play("fire_extinguish_flame", {pos = pos,max_hear_distance = 2,	gain = 0.1,})
 				if molten_sailor_mcl.durability_on and durability_seconds > 0 and (has_helmet or has_chestplate or has_pants or has_boots) then
 					for i, stack in pairs(armor_inv) do
